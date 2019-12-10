9cefa33f0118426840d2155f02269d8d82d9093b
Roman Bova <roman.bova@wallester.eu>
2019-12-10 20:34:22 +0200

    Refactor DropdownRoot helpers

--- src/components/Dropdown/DropdownRoot/DropdownRoot.test.tsx
+++ src/components/Dropdown/DropdownRoot/DropdownRoot.test.tsx
@@ -1,6 +1,6 @@
 // @ts-ignore
 
-describe('DropdownRoot hooks', () => {
+describe('DropdownRoot helpers', () => {
     test.todo('modifyDropdownOptionsDuplicates works');
     test.todo('modifyDropdownOptionsMultiline works');
 });
