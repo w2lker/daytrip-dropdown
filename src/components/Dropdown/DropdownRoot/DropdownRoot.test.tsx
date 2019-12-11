4911e9fdfb550d99ff293719f6f66d142938a81f
Roman Bova <roman.bova@wallester.eu>
2019-12-11 14:57:25 +0200

    Add tests for DropdownRoot.helpers

--- src/components/Dropdown/DropdownRoot/DropdownRoot.test.tsx
+++ src/components/Dropdown/DropdownRoot/DropdownRoot.test.tsx
@@ -1,23 +1,46 @@
-// @ts-ignore
+import {modifyDropdownOptionsDuplicates, modifyDropdownOptionsMultiline} from "./DropdownRoot.helpers";
+import {IDropdownOptionsArray} from "./DropdownRoot";
+import {getEntries} from "../../../utils/dropdown";
 
 describe('DropdownRoot helpers', () => {
-    test.todo('modifyDropdownOptionsDuplicates works');
-    test.todo('modifyDropdownOptionsMultiline works');
+  it('modifyDropdownOptionsDuplicates works', () => {
+      const testArray: IDropdownOptionsArray = [
+        {'key1': 'value1'},
+        {'key1': 'value1'},
+        {'key1': 'value1'},
+        {'key2': 'value2'},
+      ];
+      const outputArray = modifyDropdownOptionsDuplicates(testArray);
+      expect(outputArray.length).toBe(2);
+  });
+  it('modifyDropdownOptionsMultiline works', () => {
+    const expectedArray: IDropdownOptionsArray = [
+      {'key1': 'value1 value1 value1 value1'},
+      {'key2': 'value1 value1 value1 value1'},
+    ];
+    const inputArray = expectedArray.map( item => {
+      const {key, value} = getEntries(item);
+      return {[key]: value.replace(' ', ' \n')}
+    });
+    const outputArray = modifyDropdownOptionsMultiline(inputArray);
+    outputArray.forEach( (item, index) => {
+      expect(item).toEqual(expectedArray[index]);
+    });
+  });
 });
 
 describe('DropdownRoot component', () => {
-    it('some test', () => {
-        expect(true).toBe(true);
-    });
-    test.todo('match snapshot');
-    // TODO: think about other unit tests
+  it('some test', () => {
+    expect(true).toBe(true);
+  });
+  test.todo('match snapshot');
 });
 
 describe('DropdownRoot styles', () => {
-    test.todo('Some test');
+  test.todo('Some test');
 });
 
 describe('Dropdown decorators', () => {
-    test.todo('withStyles paths classes prop');
-    test.todo('decorator paths controlled props');
+  test.todo('withStyles paths classes prop');
+  test.todo('decorator paths controlled props');
 });
