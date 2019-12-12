c5b7b9b64aa70ff2a05322714b0a0febb8e4cb13
Roman Bova <roman.bova@wallester.eu>
2019-12-12 13:11:05 +0200

    Added DropdownBody basic styles added

--- src/components/Dropdown/DropdownBody/DropdownBody.styles.ts
+++ src/components/Dropdown/DropdownBody/DropdownBody.styles.ts
@@ -1,11 +1,13 @@
 const dropdownBodyStyles = {
     wrapper: {
-      padding: 0,
+      backgroundColor: '#ECF0F2',
+      borderRadius: 4,
+      boxShadow: '0 2px 5px 0 rgba(146,166,176,0.50)',
     },
     contentWrapper: {
       overflowX: 'hidden',
       overflowY: 'scroll',
-      maxHeight: 'calc(100vh - 300px)'
+      maxHeight: 'calc(100vh - 300px)',
     },
 };
 
