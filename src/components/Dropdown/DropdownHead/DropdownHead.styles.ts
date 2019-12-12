b1ad7fd00747c0dc3d535fdcb1a325dcb4254d47
Roman Bova <roman.bova@wallester.eu>
2019-12-12 11:31:46 +0200

    Add basic styles for DropdownHead component

--- src/components/Dropdown/DropdownHead/DropdownHead.styles.ts
+++ src/components/Dropdown/DropdownHead/DropdownHead.styles.ts
@@ -1,18 +1,32 @@
 const dropdownHeadStyles = {
   wrapper: {
-    padding: 0
+    background: '#ECF0F2',
+    boxShadow: '0 2px 9px 0 rgba(146,166,176,0.15)',
+    borderRadius: 4,
+    position: 'relative',
+    padding: '16px 30px'
   },
   label: {
-    padding: 0
+    paddingBottom: 6,
+    fontWeight: 500,
+    fontSize: 10,
+    color: '#445A64'
   },
   content: {
-    padding: 0
+    fontSize: 16,
+    fontWeight: 400,
+    color: '#7A929E',
+    letterSpacing: 0,
   },
   caret: {
-    padding: 0
+    position: 'absolute',
+    right: 15,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    transition: 'transform 0.3s ease-in-out'
   },
   caretReverted: {
-    transform: 'rotate(-180deg)'
+    transform: 'translateY(-50%) rotate(-180deg)'
   }
 };
 
