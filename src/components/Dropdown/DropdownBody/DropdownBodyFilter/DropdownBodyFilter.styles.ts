fe2ee7bdb0b95c744402328eae6df01f061dfb93
Roman Bova <roman.bova@wallester.eu>
2019-12-12 13:54:21 +0200

    DropdownBodyFilter component styles added

--- src/components/Dropdown/DropdownBody/DropdownBodyFilter/DropdownBodyFilter.styles.ts
+++ src/components/Dropdown/DropdownBody/DropdownBodyFilter/DropdownBodyFilter.styles.ts
@@ -1,14 +1,36 @@
 const dropdownBodyFilterStyles = {
   wrapper: {
-    padding: 0,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'flex-start',
+    paddingBottom: 12
   },
   magnify: {
-
+    lineHeight: '14px',
   },
   input: {
-    padding: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    margin: '0 16px 0 15px',
+    border: 0,
+    fontSize: 14,
+    color: '#445A65',
+    backgroundColor: 'transparent',
+    '&:focus': {
+      outline: 'none'
+    },
+    '&::placeholder': {
+      color: '#7A929E',
+    },
   },
   divider: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 2,
+    backgroundColor: '#D0D9DD',
+    opacity: .5
 
   },
 };
