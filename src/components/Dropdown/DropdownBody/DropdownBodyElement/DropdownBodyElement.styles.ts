1e5080c6a2837397a12597600edca2750674baa9
Roman Bova <roman.bova@wallester.eu>
2019-12-12 15:15:05 +0200

    Updated styles for DropdownBodyElement

--- src/components/Dropdown/DropdownBody/DropdownBodyElement/DropdownBodyElement.styles.ts
+++ src/components/Dropdown/DropdownBody/DropdownBodyElement/DropdownBodyElement.styles.ts
@@ -3,13 +3,30 @@ const dropdownBodyElementStyles = {
     fontWeight: 400,
     fontSize: 14,
     color: '#546F7B',
-    padding:'0 16px 13px 29px',
-  },
-  selected: {
-    color: 'red',
+    padding:'10px 15px 10px 45px',
+    transition: 'background-color 0.3s ease-in-out, color 0.3s ease-in',
+    '&:hover': {
+      backgroundColor: 'rgba(208,217,221, .3)',
+      cursor: 'pointer'
+    },
+    '&:first-child': {
+      marginTop: 15,
+    },
+    '&:last-child': {
+      marginBottom: 15,
+    }
   },
   focused: {
-    color: 'blue',
+    color: '#445A64',
+    backgroundColor: 'rgba(208,217,221, .3)',
+    '&:hover': {
+      color: '#304048',
+      backgroundColor: 'rgba(208,217,221, .6)',
+    }
+  },
+  selected: {
+    color: '#299BF6',
+    fontWeight: 500,
   }
 };
 
